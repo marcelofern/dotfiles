18def47f68c049b90b8ab1d36173b2ba1b6650b3
Marcelo <marceelofernandes@gmail.com>
2020-01-31 06:26:56 +1300

    adds zshrc / updates gitignore / adds more dependencies

---
+++ .zshrc
@@ -0,0 +1,40 @@
+#!/bin/zsh
+
+############ - ENV variables - ############
+export ZSH="/home/x/.oh-my-zsh"
+export MANWIDTH=90
+export HISTSIZE=10000
+export SAVEHIST=10000
+
+############ - ZSH options - ############
+ZSH_THEME="robbyrussell"
+CASE_SENSITIVE="true"
+HYPHEN_INSENSITIVE="true"
+DISABLE_AUTO_UPDATE="true"
+COMPLETION_WAITING_DOTS="true"
+HIST_STAMPS="yyyy-mm-dd"
+SIMPL_ZSH_DIR=$HOME/.zsh
+plugins=(git)
+source $ZSH/oh-my-zsh.sh
+
+############ - User configuration - ############
+
+export LANG=en_US.UTF-8
+export EDITOR='nvim'
+
+KEYTIMEOUT=1  # Fixes slow <esc> in vim
+stty -ixon # don't disturbe when CTRL+S and CTRL+Q
+stty sane # avoid tmux going crazy
+
+############ - Aliases - ############
+alias vim='nvim'
+alias vimrc="vim ~/.config/nvim/init.vim"
+alias zshrc="vim ~/.zshrc"
+alias tmuxrc="vim ~/.tmux.conf"
+alias i3rc="vim ~/.config/i3/config"
+alias polybarrc="vim ~/.config/polybar/config"
+alias brightdown="xbacklight -dec 10"
+alias brightup="xbacklight -inc 10"
+alias update="source ~/.zshrc"
+
+clear
--- install.sh
+++ install.sh
@@ -11,31 +11,41 @@
 #  cp /etc/X11/xinit/xinitrc ~/.xinitrc and remove the xterm lines and add "exec i3"
 
 # General dependencies.
-sudo pacman -S i3-gaps xorg-server xorg-xinit xorg-xrandr zsh neovim dmenu 
-               git termite openssh base-devel
+sudo pacman -S i3-gaps xorg-server xorg-xinit xorg-xrandr xorg-xbacklight zsh neovim dmenu 
+sudo pacman -S git termite openssh base-devel
 
 # Fonts
 sudo pacman -S ttf-dejavu noto-fonts ttf-liberation ttf-hack 
-               ttf-linux-libertine noto-fonts-emoji font-mathematica
-               ttf-croscore
+sudo pacman -S ttf-linux-libertine noto-fonts-emoji font-mathematica ttf-croscore
 
 # Polybar dependencies.
 sudo pacman -S cairo libxcb python xcb-proto xcb-util-image 
-               xcb-util-wm xcb-util-cursor xcb-util-xrm alsa-lib libpulse jsoncpp
-               libmpdclient curl libnl wireless_tools 
+sudo pacman -S xcb-util-wm xcb-util-cursor xcb-util-xrm alsa-lib libpulse jsoncpp
+sudo pacman -S libmpdclient curl libnl wireless_tools 
                
 # Video driver
 pacman -S xf86-video-intel
+
+# misc
+sudo pacman -S neofetch
 
 # Install yay
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+cd ~
 
 # AUR dependencies
 yay polybar
 yay ttf-google-fonts-typewolf
 yay pamac-aur
 
+# Zsh as shell (opts, will have to restart the computer to take effect)
+chsh -l
+chsh -s $(which zsh) $(whoami)
 
-# wget https://raw.githubusercontent.com/marcelodufe/environment/master/i3.config
+# install oh my zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Prevent modificiations from installation process to change the repo.
+git checkout .
