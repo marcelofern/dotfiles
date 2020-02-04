f80ed471b385fa7ce76691cb2588f32ee990017b
Marcelo <marceelofernandes@gmail.com>
2020-02-04 14:02:19 +1300

    Clean up of install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,39 @@
-# Install arch via anarchy and add the internet module (networkmanager)
+# Install arch and add the internet module (networkmanager)
 # To use wifi after installation:
 # sudo systemctl enable NetworkManager
 # sudo systemctl start NetworkManager
 # nmtui
 
-# General dependencies.
-sudo pacman -S i3-gaps xorg-server xorg-xinit xorg-xrandr xorg-xbacklight zsh neovim dmenu 
-sudo pacman -S git termite openssh base-devel nitrogen alsa-utils pulseaudio htop neofetch
-sudo pacman -S xf86-video-intel imagemagick rofi xorg-xev bluez bluez-utils i3lock which
-sudo pacman -S tree
+echo "Downloading general dependencies..."
+sudo pacman -S \
+  i3-gaps xorg-server xorg-xinit xorg-xrandr \
+  xorg-xbacklight zsh neovim dmenu \
+  git termite openssh base-devel \
+  nitrogen alsa-utils pulseaudio \
+  htop neofetch xf86-video-intel \
+  imagemagick rofi xorg-xev bluez \
+  bluez-utils i3lock which tree
 
-# Fonts
-sudo pacman -S ttf-dejavu noto-fonts ttf-liberation ttf-hack ttf-font-awesome
-sudo pacman -S ttf-linux-libertine noto-fonts-emoji font-mathematica ttf-croscore
+echo "Downloading fonts..."
+sudo pacman -S \
+  ttf-dejavu noto-fonts ttf-liberation \
+  ttf-hack ttf-font-awesome ttf-linux-libertine \
+  noto-fonts-emoji font-mathematica ttf-croscore
 
-# Polybar dependencies.
-sudo pacman -S cairo libxcb python xcb-proto xcb-util-image
-sudo pacman -S xcb-util-wm xcb-util-cursor xcb-util-xrm alsa-lib libpulse jsoncpp
-sudo pacman -S libmpdclient curl libnl wireless_tools 
+echo "Downloading Polybar dependencies..."
+sudo pacman -S \
+  cairo libxcb python xcb-proto \
+  xcb-util-image xcb-util-wm xcb-util-cursor \
+  xcb-util-xrm alsa-lib libpulse jsoncpp \
+  libmpdclient curl libnl wireless_tools
 
-# YAY
+echo "Downloading YAY"
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd ..
 
-# AUR dependencies
+echo "Downloading AUR dependencies"
 yay polybar
 yay ttf-google-fonts-typewolf
 yay ttf-material-design-icons
@@ -37,25 +45,22 @@ yay flameshot
 yay peek
 yay tty-clock
 
-# Oh my zsh
+echo "Downloading oh my zsh"
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Neo vim
+echo "Downloading neovim dependencies"
 sudo pacman -S ack ripgrep
 sudo pip install --upgrade neovim
 sudo pip install --upgrade jedi
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Color scheme
+echo "Applying gruvbox color schema"
 wal --theme base16-gruvbox-soft
 
-# generate ssh key
+echo "Generating .ssh/ folder"
 ssh-keygen
 
-# Zsh as shell (You will have to restart the computer to take effect)
+echo "Using zsh as a shell. Note: Restart your computer to take effect"
 chsh -l
 chsh -s $(which zsh) $(whoami)
-
-# Prevent modificiations from installation process to change the repo.
-git checkout .
